9241c38f69be2a1cca77d04da4f7545eaa123831
Niwresh <sherwinlumakang827@gmail.com>
2025-09-21 09:03:40 +0800

    Added home tabs

---
+++ src/pages/Home-tabs/Maps.tsx
@@ -0,0 +1,28 @@
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Maps: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Maps</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Maps;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,54 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
+import {  
+      IonIcon, 
+      IonLabel,  
+      IonRouterOutlet, 
+      IonTabBar, 
+      IonTabButton, 
+      IonTabs, 
+      
   } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { bookOutline, search, star } from 'ionicons/icons';
+import { Route, Redirect } from 'react-router';
+
+import Dashboard from './Home-tabs/Dashboard';
+import Maps from './Home-tabs/Maps';
+import Alerts from './Home-tabs/Alerts';
   
   const Home: React.FC = () => {
+
+    const tabs = [
+      {name:'Dashboard', tab:'dashboard',url: '/EmergTrack/app/home/dashboard', icon: bookOutline},
+      {name:'Maps', tab:'maps', url: '/EmergTrack/app/home/maps', icon: search},
+      {name:'Alerts',tab:'alerts', url: '/EmergTrack/app/home/alerts', icon: star},
+    ]
+    
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Template</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
+      <IonReactRouter>
+        <IonTabs>
+          <IonTabBar slot="bottom">
+
+            {tabs.map((item, index) => (
+              <IonTabButton key={index} tab={item.tab} href={item.url}>
+                <IonIcon icon={item.icon} />
+                <IonLabel>{item.name}</IonLabel>
+              </IonTabButton>
+            ))}
+            
+          </IonTabBar>
+        <IonRouterOutlet>
+
+          <Route exact path="/EmergTrack/app/home/dashboard" component={Dashboard} />
+          <Route exact path="/EmergTrack/app/home/maps" component={Maps} />
+          <Route exact path="/EmergTrack/app/home/alerts" component={Alerts} />
+
+          <Route exact path="/EmergTrack/app/home">
+            <Redirect to="/EmergTrack/app/home/dashboard" />
+          </Route>
+
+        </IonRouterOutlet>
+        </IonTabs>
+      </IonReactRouter>
     );
   };
   
