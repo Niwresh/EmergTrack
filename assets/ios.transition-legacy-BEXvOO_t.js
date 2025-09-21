03c1ca877d2d99380868a12b322c948a62bb7ed3
Niwresh <sherwinlumakang827@gmail.com>
2025-09-21 11:54:28 +0800

    Updates

--- assets/ios.transition-legacy-BEXvOO_t.js
+++ assets/ios.transition-legacy-BEXvOO_t.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BOMkzZBA.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-C4p2TIH4.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-BIhdv2Xi.js
+++ assets/md.transition-legacy-BIhdv2Xi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BOMkzZBA.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-C4p2TIH4.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
