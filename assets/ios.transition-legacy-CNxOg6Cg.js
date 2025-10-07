e3cb4a951c12da42dcb733399695f7b7a48862d3
Niwresh <sherwinlumakang827@gmail.com>
2025-10-08 06:29:42 +0800

    Updates

--- assets/ios.transition-legacy-CNxOg6Cg.js
+++ assets/ios.transition-legacy-CNxOg6Cg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BF5gzKd2.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C69LsnS2.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-BUq63sPt.js
+++ assets/status-tap-legacy-BUq63sPt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BF5gzKd2.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-C69LsnS2.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
