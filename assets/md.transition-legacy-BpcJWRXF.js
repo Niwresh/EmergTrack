66252ff9f4d82f52ddaa46c7495b1f2255fab4c9
Niwresh <sherwinlumakang827@gmail.com>
2025-10-14 12:24:45 +0800

    Updates

--- assets/md.transition-legacy-BpcJWRXF.js
+++ assets/md.transition-legacy-BpcJWRXF.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CrrhaBRW.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-aRFSHLBv.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
