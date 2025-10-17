da9d51c0b3977fab4e4b29374bf5adc5347b0b7f
Niwresh <sherwinlumakang827@gmail.com>
2025-10-18 00:43:42 +0800

    Updates

--- assets/swipe-back-legacy-DV4uQlEu.js
+++ assets/swipe-back-legacy-DV4uQlEu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BiGZDgUr.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-C9JG2TMM.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
