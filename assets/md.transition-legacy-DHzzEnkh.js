1114e52daa583030e52381702611d39db0fa8fc0
Niwresh <sherwinlumakang827@gmail.com>
2025-11-09 20:12:38 +0800

    Updates

--- assets/md.transition-legacy-DHzzEnkh.js
+++ assets/md.transition-legacy-DHzzEnkh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C9JG2TMM.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DMcaDAxF.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
