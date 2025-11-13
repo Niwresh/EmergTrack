73741789f35c10c2b159d60bc2e1f6115ed7e49d
Niwresh <sherwinlumakang827@gmail.com>
2025-11-13 16:16:59 +0800

    Updates

--- assets/md.transition-legacy-DLQj_iH3.js
+++ assets/md.transition-legacy-DLQj_iH3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DMcaDAxF.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DUZfLbVl.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
