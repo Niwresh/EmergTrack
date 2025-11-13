57b9ce536e48a1afaa02628732ebb638d5a9bd83
Niwresh <sherwinlumakang827@gmail.com>
2025-11-13 17:07:45 +0800

    Updates

--- assets/ios.transition-legacy-KZ4NT_6Y.js
+++ assets/ios.transition-legacy-KZ4NT_6Y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B-6WQFxF.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-92Os4_LV.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-MPzRA2OE.js
+++ assets/md.transition-legacy-MPzRA2OE.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B-6WQFxF.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-92Os4_LV.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
