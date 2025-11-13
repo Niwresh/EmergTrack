060aa6f1fd6d6dad4c55bcbee3a2922bc79eee7b
Niwresh <sherwinlumakang827@gmail.com>
2025-11-13 19:11:27 +0800

    Updates

--- assets/md.transition-legacy-CCWc8ATV.js
+++ assets/md.transition-legacy-CCWc8ATV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DEG9ksej.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-xX9nGQZe.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
